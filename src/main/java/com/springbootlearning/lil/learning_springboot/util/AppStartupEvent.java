2cf4fec01e7b19ddd45d705fab98087fd0e38f9c
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-09 22:40:12 +0300

    constructor generation

--- src/main/java/com/springbootlearning/lil/learning_springboot/util/AppStartupEvent.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/util/AppStartupEvent.java
@@ -1,10 +1,16 @@
 package com.springbootlearning.lil.learning_springboot.util;
 
+import com.springbootlearning.lil.learning_springboot.data.RoomRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+    private final RoomRepository roomRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
