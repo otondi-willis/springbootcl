9e15d018f97bcd89483c2b93fc30d2218eadccfa
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-09 21:51:55 +0300

    defined column Room_ID

--- src/main/java/com/springbootlearning/lil/learning_springboot/data/Room.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/data/Room.java
@@ -1,9 +1,16 @@
 package com.springbootlearning.lil.learning_springboot.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="ROOM")
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="ROOM_ID")
+    private long id;
+
+    NAME VARCHAR(16) NOT NULL,
+    ROOM_NUMBER CHAR(2) NOT NULL UNIQUE,
+    BED_INFO CHAR(2) NOT NULL
 }
