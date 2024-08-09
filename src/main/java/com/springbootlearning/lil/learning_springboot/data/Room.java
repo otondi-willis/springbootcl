0d4685e9b3bff1e0097e031adede7a3941f42919
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-09 21:47:22 +0300

    @Table annotation used with assigned table name

--- src/main/java/com/springbootlearning/lil/learning_springboot/data/Room.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/data/Room.java
@@ -1,7 +1,9 @@
 package com.springbootlearning.lil.learning_springboot.data;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="ROOM")
 public class Room {
 }
