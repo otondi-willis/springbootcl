1481a94108247870dd72fd480877c3899ebbc8d6
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-09 21:53:10 +0300

    defined column NAME

--- src/main/java/com/springbootlearning/lil/learning_springboot/data/Room.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/data/Room.java
@@ -9,8 +9,8 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="ROOM_ID")
     private long id;
-
-    NAME VARCHAR(16) NOT NULL,
+    @Column(name="NAME")
+    private String name;
     ROOM_NUMBER CHAR(2) NOT NULL UNIQUE,
     BED_INFO CHAR(2) NOT NULL
 }
