64e120300ebfb67890ca1a9d5717627862301dc1
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-10 13:26:18 +0300

    Generating Reservation table columns

--- src/main/java/com/springbootlearning/lil/learning_springboot/data/Reservation.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/data/Reservation.java
@@ -1,10 +1,13 @@
 package com.springbootlearning.lil.learning_springboot.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="RESERVATION")
 public class Reservation {
+    @Id
+    @Column(name="RESERVATI_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
 
 }
