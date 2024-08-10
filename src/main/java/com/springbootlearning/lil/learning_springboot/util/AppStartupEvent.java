8a8370ea3ca4b13878bf12d7b0843820297b1d36
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-10 15:53:44 +0300

    additional attributes in AppStartupEvent

--- src/main/java/com/springbootlearning/lil/learning_springboot/util/AppStartupEvent.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/util/AppStartupEvent.java
@@ -1,7 +1,7 @@
 package com.springbootlearning.lil.learning_springboot.util;
 
-import com.springbootlearning.lil.learning_springboot.data.Room;
-import com.springbootlearning.lil.learning_springboot.data.RoomRepository;
+import com.springbootlearning.lil.learning_springboot.data.*;
+import org.antlr.v4.runtime.tree.pattern.ParseTreePattern;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -9,14 +9,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
     private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
 
-    public AppStartupEvent(RoomRepository roomRepository) {
+    public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
+
         this.roomRepository = roomRepository;
+        this.guestRepository= guestRepository;
+        this.reservationRepository=reservationRepository;
+
     }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         Iterable<Room> rooms = this.roomRepository.findAll();
         rooms.forEach(System.out::println);
+        Iterable<Guest> guests = this.guestRepository.findAll();
+        guests.forEach(System.out::println);
+        Iterable<Reservation> reservations = this.reservationRepository.findAll();
+        reservations.forEach(System.out::println);
     }
 }
