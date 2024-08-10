693a1a3bf5283b6b590d02c5ca672fffd54d4a8e
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-10 15:41:53 +0300

    use of iterable

--- src/main/java/com/springbootlearning/lil/learning_springboot/data/ReservationRepository.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/data/ReservationRepository.java
@@ -3,6 +3,9 @@ package com.springbootlearning.lil.learning_springboot.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    Iterable<Reservation> findReservationByReservationDate(Date date);
 }
