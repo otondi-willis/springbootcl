7268d5671557fe3ae026ccb403fb6ba4178be594
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-10 13:27:17 +0300

    Generating reservationId column

--- src/main/java/com/springbootlearning/lil/learning_springboot/data/Reservation.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/data/Reservation.java
@@ -8,6 +8,10 @@ public class Reservation {
     @Id
     @Column(name="RESERVATI_ID")
     @GeneratedValue(strategy = GenerationType.AUTO)
-
+    private long reservationId;
+    RESERVATION_ID BIGSERIAL PRIMARY KEY,
+    ROOM_ID BIGINT NOT NULL,
+    GUEST_ID BIGINT NOT NULL,
+    RES_DATE DATE
 
 }
