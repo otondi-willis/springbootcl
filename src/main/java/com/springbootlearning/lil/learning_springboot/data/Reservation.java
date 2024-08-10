bb4303b71c5d992572dab20b8ba7517523f39393
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-10 13:24:05 +0300

    use of @Entity annotation

--- src/main/java/com/springbootlearning/lil/learning_springboot/data/Reservation.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/data/Reservation.java
@@ -1,5 +1,10 @@
 package com.springbootlearning.lil.learning_springboot.data;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="RESERVATION")
 public class Reservation {
 
 }
