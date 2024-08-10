ca9886a8c0efa81b52a4931158ad45c97dae6ffd
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-10 16:08:25 +0300

    GuestRepository interface

---
+++ src/main/java/objectwithspring/data/GuestRepository.java
@@ -0,0 +1,8 @@
+package objectwithspring.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+}
