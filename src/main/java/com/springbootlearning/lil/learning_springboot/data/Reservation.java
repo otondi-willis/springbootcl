a8518845df2439a6ddac35ad0f3c01692a2f48f5
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-10 13:34:07 +0300

    Generating getters and setters

--- src/main/java/com/springbootlearning/lil/learning_springboot/data/Reservation.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/data/Reservation.java
@@ -19,4 +19,35 @@ public class Reservation {
     @Column(name="RES_DATE")
     private Date reservationDate;
 
+    public long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(long guestId) {
+        this.guestId = guestId;
+    }
+
+    public Date getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(Date reservationDate) {
+        this.reservationDate = reservationDate;
+    }
 }
