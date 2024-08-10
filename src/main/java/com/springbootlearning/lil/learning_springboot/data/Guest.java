a77ab5d78c968ca091957a7f9605931b8e2e0809
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-10 13:07:29 +0300

    instantiating a table GUEST

--- src/main/java/com/springbootlearning/lil/learning_springboot/data/Guest.java
+++ src/main/java/com/springbootlearning/lil/learning_springboot/data/Guest.java
@@ -1,5 +1,17 @@
 package com.springbootlearning.lil.learning_springboot.data;
 
-public class Guest {
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+public class Guest {
+    @Id
+    @Table(name="GUEST")
+    GUEST_ID BIGSERIAL PRIMARY KEY,
+    FIRST_NAME VARCHAR(64),
+    LAST_NAME VARCHAR(64),
+    EMAIL_ADDRESS VARCHAR(64),
+    ADDRESS VARCHAR(64),
+    COUNTRY VARCHAR(32),
+    STATE VARCHAR(12),
+    PHONE_NUMBER VARCHAR(24)
 }
