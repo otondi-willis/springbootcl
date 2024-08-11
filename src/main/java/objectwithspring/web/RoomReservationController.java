04ba6d023ca9c05f115c68137c26ba210c923597
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-11 11:38:34 +0300

    private final attributes and constructor

--- src/main/java/objectwithspring/web/RoomReservationController.java
+++ src/main/java/objectwithspring/web/RoomReservationController.java
@@ -1,9 +1,19 @@
 package objectwithspring.web;
 
+import objectwithspring.business.ReservationService;
+import objectwithspring.util.DateUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/reservations")
 public class RoomReservationController {
+
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public RoomReservationController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
 }
