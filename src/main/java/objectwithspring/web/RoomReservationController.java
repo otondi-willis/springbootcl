26a51b9adb2beccbf56bb3bcbf74eb6b4c721c13
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-11 11:46:59 +0300

    added functionality to getReservation method

--- src/main/java/objectwithspring/web/RoomReservationController.java
+++ src/main/java/objectwithspring/web/RoomReservationController.java
@@ -1,12 +1,16 @@
 package objectwithspring.web;
 
 import objectwithspring.business.ReservationService;
+import objectwithspring.business.RoomReservation;
 import objectwithspring.util.DateUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/reservations")
@@ -21,6 +25,10 @@ public class RoomReservationController {
     }
     @RequestMapping(method = RequestMethod.GET)
     public String getReservation(@RequestParam(value="date", required=false)String dateString, Model model){
+        Date date = this.dateUtils.createDateFromDateString(dateString);
+        List<RoomReservation> roomReservations = this.reservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservations", roomReservations);
+        return "roomres";
 
     }
 }
