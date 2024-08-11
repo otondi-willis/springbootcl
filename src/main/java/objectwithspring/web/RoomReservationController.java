08045dbb391ca6b352a53e5b87553ec5712da05b
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-11 11:42:41 +0300

    use @RequestParam

--- src/main/java/objectwithspring/web/RoomReservationController.java
+++ src/main/java/objectwithspring/web/RoomReservationController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/reservations")
@@ -19,7 +20,7 @@ public class RoomReservationController {
         this.reservationService = reservationService;
     }
     @RequestMapping(method = RequestMethod.GET)
-    public String getReservation(String dateString, Model model){
+    public String getReservation(@RequestParam(value="date", required=false)String dateString, Model model){
 
     }
 }
