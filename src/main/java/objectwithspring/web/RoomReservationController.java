ecdc402c6c49773580f455382230aed7effe9a1a
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-11 11:27:33 +0300

    use of @RequestMapping

--- src/main/java/objectwithspring/web/RoomReservationController.java
+++ src/main/java/objectwithspring/web/RoomReservationController.java
@@ -1,8 +1,9 @@
 package objectwithspring.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/reservations")
 public class RoomReservationController {
 }
