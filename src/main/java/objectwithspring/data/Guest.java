7f374d6a74fc7a2089fba888e96fd30161f7e4d0
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-11 11:03:28 +0300

    debugged Guest class

--- src/main/java/objectwithspring/data/Guest.java
+++ src/main/java/objectwithspring/data/Guest.java
@@ -1,11 +1,7 @@
 package objectwithspring.data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="GUEST")
