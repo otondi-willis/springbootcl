4ebe5d9937f8da8a510bab6136e0a2058b3c5cc4
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-11 11:41:21 +0300

    getReservation method created

--- src/main/java/objectwithspring/web/RoomReservationController.java
+++ src/main/java/objectwithspring/web/RoomReservationController.java
@@ -3,7 +3,9 @@ package objectwithspring.web;
 import objectwithspring.business.ReservationService;
 import objectwithspring.util.DateUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/reservations")
@@ -15,5 +17,9 @@ public class RoomReservationController {
     public RoomReservationController(DateUtils dateUtils, ReservationService reservationService) {
         this.dateUtils = dateUtils;
         this.reservationService = reservationService;
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public String getReservation(String dateString, Model model){
+
     }
 }
