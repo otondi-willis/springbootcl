730b65f4e3670f6c79c2be605e4d157222e27bd8
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-12 04:32:08 +0300

    attributes and a constructor

--- src/main/java/objectwithspring/webservice/WebServiceController.java
+++ src/main/java/objectwithspring/webservice/WebServiceController.java
@@ -1,10 +1,18 @@
 package objectwithspring.webservice;
 
+import objectwithspring.business.ReservationService;
+import objectwithspring.util.DateUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class WebServiceController {
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
 
+    public WebServiceController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
 }
