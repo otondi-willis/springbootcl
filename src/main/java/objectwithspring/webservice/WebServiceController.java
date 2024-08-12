baeb186a37a3d06224375fdec3229c7d97301aae
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-12 04:35:58 +0300

    create getReservation method

--- src/main/java/objectwithspring/webservice/WebServiceController.java
+++ src/main/java/objectwithspring/webservice/WebServiceController.java
@@ -1,9 +1,14 @@
 package objectwithspring.webservice;
 
 import objectwithspring.business.ReservationService;
+import objectwithspring.business.RoomReservation;
 import objectwithspring.util.DateUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -14,5 +19,10 @@ public class WebServiceController {
     public WebServiceController(DateUtils dateUtils, ReservationService reservationService) {
         this.dateUtils = dateUtils;
         this.reservationService = reservationService;
+    }
+
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<RoomReservation> getReservation(@RequestParam(value = "date", required = false)String dateString){
+
     }
 }
