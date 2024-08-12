3db1db399a7476e0a160dbc4e877fcdaa6a6be2f
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-12 04:30:07 +0300

    use of @RequestMapping annotation

--- src/main/java/objectwithspring/webservice/WebServiceController.java
+++ src/main/java/objectwithspring/webservice/WebServiceController.java
@@ -1,7 +1,10 @@
 package objectwithspring.webservice;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class WebServiceController {
+
 }
